53fa4be8cc6463048c597c58b58ee28d50555861
Lu <lokka.rei@yandex.ru>
2022-07-19 15:35:45 +0300

    готовое1

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,26 @@ public class Main {
         List<Integer> nums = new ArrayList<>(List.of(6, 9, 11, 45, 3, 44, 22));
         for (int i = 0; i < nums.size(); i++) {
             if (nums.get(i) % 2 != 0) {
-                System.out.print(nums.get(i) + " ");
+                System.out.println(nums.get(i) + " ");
             }
         }
 
         List<Integer> num = new ArrayList<>(List.of(1, 1, 8, 3, 4, 4, 5, 5, 2, 7));
-        Set<Integer> number = new HashSet<>();
+        Collections.sort(num);
+        Integer q = null;
         for (int i = 0; i < num.size(); i++) {
-            if (num.get(i) % 2 == 0) {
-                number.add(num.get(i));
+            if (num.get(i) % 2 == 0 && num.get(i) != q) {
+                System.out.println(num.get(i));
+                q = num.get(i);
             }
         }
-        System.out.println(number + " ");
 
         List<String> word = new ArrayList<>(List.of("dog", "cat", "dog", "mouse"));
-        Set<String> words = new HashSet<>();
-        for (int i = 0; i < word.size(); i++) {
-            words.add(word.get(i));
-        }
+        Set<String> words = new HashSet<>(word);
         System.out.println(words);
 
         List<String> wordS = new ArrayList<>(List.of("dog", "cat", "dog", "mouse"));
-        int resultWord = Collections.frequency(wordS, "dog" );
-        System.out.println(resultWord);
+        Set<String> wordsUnique = new HashSet<>(word);
+        System.out.println(wordS.size() - wordsUnique.size());
 
     }}
